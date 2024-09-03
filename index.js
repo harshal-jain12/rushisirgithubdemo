ef170da3915c9203b359d386adc01ddc196744ca
harshal <harshalgosavi334@gmail.com>
2024-09-03 17:25:09 +0530

    Modified revert

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const name = "Ram Bharoshe";
 const dob = "03/09/2024";
 
 const isAdult = () => {
-  return age >= 18;
-  console.log(
-    "isAdult called with     " + age + " years old and   " + age + " years old");
+  if (age >= 18) {
+    console.log("Adult");
+  } else {
+    console.log("children");
+  }
 };
